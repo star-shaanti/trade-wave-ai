91ee927ee397e2771def9347c94a3d5f68365d1d
Jes1985 <votre-email@example.com>
2025-11-15 14:59:24 +0100

    chore: remove header ad

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -22,7 +22,7 @@ import { LanguageSelectorSimple } from "../components/LanguageSelectorSimple";
 import { LanguageSelectorFixed } from "../components/LanguageSelectorFixed";
 import { LanguageButton } from "../components/LanguageButton";
 import { useLanguage } from "../hooks/useLanguage";
-import { HeaderAd, FooterAd, SidebarAdLeft, SidebarAdRight } from "../components/AdSense";
+import { HeaderAd, FooterAd, SidebarAd } from "../components/AdSense";
 
 // Constantes pour l'API Gemini
 const GEMINI_API_KEY = "AIzaSyAglyLqDVp1v9JQT2z27Z1-F1LddnB9_Mk";
@@ -1062,7 +1062,10 @@ const Index = () => {
         </div>
       </header>
 
-      {/* Publicité en-tête désactivée */}
+      {/* Publicité en-tête */}
+      <div className="w-full bg-gray-50 dark:bg-gray-800 py-2">
+        <HeaderAd />
+      </div>
 
       {/* Modal d'authentification */}
       <Dialog open={showAuthModal} onOpenChange={setShowAuthModal}>
